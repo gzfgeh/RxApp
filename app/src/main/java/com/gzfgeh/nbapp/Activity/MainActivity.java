dc208dd45639521f000ddeebf82e5fb58927c887
guzhenfu <guzhenfu@shcem.com>
2016-10-24 18:03:43 +0800

    add bottom navigation

--- app/src/main/java/com/gzfgeh/nbapp/Activity/MainActivity.java
+++ app/src/main/java/com/gzfgeh/nbapp/Activity/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationB
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_main);
         BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
         bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
         bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE);
