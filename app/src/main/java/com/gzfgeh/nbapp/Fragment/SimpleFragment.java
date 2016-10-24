67bf3af328277eb2970119259d94f149117ec716
guzhenfu <guzhenfu@shcem.com>
2016-10-24 15:38:41 +0800

    add btn circle bg

--- app/src/main/java/com/gzfgeh/nbapp/Fragment/SimpleFragment.java
+++ app/src/main/java/com/gzfgeh/nbapp/Fragment/SimpleFragment.java
@@ -7,10 +7,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.gzfgeh.nbapp.Activity.MainActivity;
-import com.gzfgeh.nbapp.Activity.SplashActivity;
 import com.gzfgeh.nbapp.Interface.LauncherBaseFragment;
 import com.gzfgeh.nbapp.R;
 import com.gzfgeh.nbapp.Utils.ShareUtils;
