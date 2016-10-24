5a65078ec541b68b3bcf2c653116bf8d8d94f088
guzhenfu <guzhenfu@shcem.com>
2016-10-24 16:38:48 +0800

    finish launch activity

--- app/src/main/java/com/gzfgeh/nbapp/Activity/MainActivity.java
+++ app/src/main/java/com/gzfgeh/nbapp/Activity/MainActivity.java
@@ -1,15 +1,94 @@
 package com.gzfgeh.nbapp.Activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.gzfgeh.nbapp.Fragment.HomeFragment;
 import com.gzfgeh.nbapp.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
+    private ArrayList<Fragment> fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE);
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.home, "Home").setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.drawable.besttrade_a, "Books").setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.drawable.consult_a, "Video").setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.drawable.myorder_a, "Music").setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.drawable.my_a, "Movies").setActiveColorResource(R.color.colorPrimary))
+                .setFirstSelectedPosition(0)
+                .initialise();
+
+        fragments = getFragments();
+        setDefaultFragment();
+        bottomNavigationBar.setTabSelectedListener(this);
+    }
+
+    /**
+     * 设置默认的
+     */
+    private void setDefaultFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.layFrame, new HomeFragment());
+        transaction.commit();
+    }
+
+    private ArrayList<Fragment> getFragments() {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new HomeFragment());
+        fragments.add(new HomeFragment());
+        fragments.add(new HomeFragment());
+        fragments.add(new HomeFragment());
+        fragments.add(new HomeFragment());
+        return fragments;
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+        if (fragments != null) {
+            if (position < fragments.size()) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                Fragment fragment = fragments.get(position);
+                if (fragment.isAdded()) {
+                    ft.replace(R.id.layFrame, fragment);
+                } else {
+                    ft.add(R.id.layFrame, fragment);
+                }
+                ft.commitAllowingStateLoss();
+            }
+        }
+
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+        if (fragments != null) {
+            if (position < fragments.size()) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                Fragment fragment = fragments.get(position);
+                ft.remove(fragment);
+                ft.commitAllowingStateLoss();
+            }
+        }
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
     }
 }
