8884ead6010f8418b7d695b428d311ecc7ce6b55
guzhenfu <guzhenfu@shcem.com>
2016-11-02 16:56:37 +0800

    add intRecyclerView

--- app/src/main/java/com/gzfgeh/nbapp/Fragment/TabLayoutFragment.java
+++ app/src/main/java/com/gzfgeh/nbapp/Fragment/TabLayoutFragment.java
@@ -3,11 +3,15 @@ package com.gzfgeh.nbapp.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gzfgeh.GRecyclerView;
+import com.gzfgeh.adapter.BaseViewHolder;
+import com.gzfgeh.adapter.RecyclerArrayAdapter;
+import com.gzfgeh.nbapp.Bean.DataBean;
 import com.gzfgeh.nbapp.R;
 
 import butterknife.BindView;
@@ -18,11 +22,14 @@ import butterknife.ButterKnife;
  * Use the {@link TabLayoutFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TabLayoutFragment extends Fragment {
+public class TabLayoutFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, RecyclerArrayAdapter.OnLoadMoreListener {
     private static final String ARG_PARAM1 = "param1";
+    private String mParam1;
+
     @BindView(R.id.recyclerView)
     GRecyclerView recyclerView;
-    private String mParam1;
+
+    private RecyclerArrayAdapter<DataBean> adapter;
 
     public static TabLayoutFragment newInstance(String param1) {
         TabLayoutFragment fragment = new TabLayoutFragment();
@@ -45,7 +52,31 @@ public class TabLayoutFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tab_layout, container, false);
         ButterKnife.bind(this, view);
+        initRecyclerView();
         return view;
     }
 
+    private void initRecyclerView() {
+        adapter = new RecyclerArrayAdapter<DataBean>(getContext(), R.layout.item_news) {
+            @Override
+            protected void convert(BaseViewHolder baseViewHolder, DataBean dataBean) {
+                baseViewHolder.setImageUrl(R.id.image_id, dataBean.getThumbnail_pic_s(), R.mipmap.ic_launcher);
+                baseViewHolder.setText(R.id.title_id, dataBean.getTitle());
+                baseViewHolder.setText(R.id.time_id, dataBean.getDate());
+            }
+        };
+
+        recyclerView.setAdapterDefaultConfig(adapter, this, this);
+        onRefresh();
+    }
+
+    @Override
+    public void onRefresh() {
+
+    }
+
+    @Override
+    public void onLoadMore() {
+
+    }
 }
