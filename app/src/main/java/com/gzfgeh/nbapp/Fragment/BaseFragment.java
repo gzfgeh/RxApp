fd890a45833a8abd63f949e3805779ceefe02dee
guzhenfu <guzhenfu@shcem.com>
2016-11-14 18:44:54 +0800

    add pull to zoom

--- app/src/main/java/com/gzfgeh/nbapp/Fragment/BaseFragment.java
+++ app/src/main/java/com/gzfgeh/nbapp/Fragment/BaseFragment.java
@@ -18,6 +18,26 @@ import com.gzfgeh.nbapp.Utils.ShareUtils;
 public class BaseFragment extends Fragment {
     private ActivityComponent activityComponent;
 
+    private static final String ARG_PARAM1 = "param1";
+    protected String mParam1;
+
+    public static MyFragment newInstance(String param1) {
+        MyFragment fragment = new MyFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+
     public ActivityComponent getActivityComponent() {
         if (activityComponent == null) {
             activityComponent = ActivityComponentFactory.create(getActivity());
--- app/src/main/java/com/gzfgeh/nbapp/Fragment/MyFragment.java
+++ app/src/main/java/com/gzfgeh/nbapp/Fragment/MyFragment.java
@@ -1,20 +1,14 @@
 package com.gzfgeh.nbapp.Fragment;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Toast;
 
-import com.gzfgeh.nbapp.Activity.SettingsActivity;
 import com.gzfgeh.nbapp.R;
+import com.gzfgeh.pullToZoom.PullToZoomScrollViewEx;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,34 +19,15 @@ import butterknife.ButterKnife;
  * create an instance of this fragment.
  */
 public class MyFragment extends BaseFragment {
-    private static final String ARG_PARAM1 = "param1";
-    @BindView(R.id.btn)
-    Button button;
-    private String mParam1;
-
-    public static MyFragment newInstance(String param1) {
-        MyFragment fragment = new MyFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-        }
-    }
+    @BindView(R.id.scroll_view)
+    PullToZoomScrollViewEx scrollView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my, container, false);
         ButterKnife.bind(this, view);
-        button.setOnClickListener(view1 ->
-                startActivity(new Intent(getActivity(), SettingsActivity.class)));
+
         return view;
     }
 
