be6df73f612fa4441ce4a32133b7b522c5e991fe
guzhenfu <gzfgeh@gmail.com>
2017-03-20 23:41:13 +0800

    init react native

--- app/src/main/java/com/gzfgeh/nbapp/Fragment/SimpleFragment.java
+++ app/src/main/java/com/gzfgeh/nbapp/Fragment/SimpleFragment.java
@@ -1,34 +1,39 @@
 package com.gzfgeh.nbapp.Fragment;
 
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.gzfgeh.nbapp.Activity.MainActivity;
 import com.gzfgeh.nbapp.Interface.LauncherBaseFragment;
 import com.gzfgeh.nbapp.R;
 import com.gzfgeh.nbapp.Utils.ShareUtils;
-import com.gzfgeh.nbapp.databinding.FragmentLaunchBinding;
+
+import butterknife.BindView;
 
 /**
  * Created by guzhenfu on 16/6/6.
  */
 public class SimpleFragment extends LauncherBaseFragment {
+    @BindView(R.id.btn)
+    Button btn;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        FragmentLaunchBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_launch, container, false);
-        binding.setBtnText("立即使用");
-        binding.setBtnClick(View-> {
+        View view = inflater.inflate(R.layout.fragment_launch, container, false);
+
+        btn.setOnClickListener(View -> {
             ShareUtils.putValue("isComeOver", true);
             startActivity(new Intent(getActivity(), MainActivity.class));
             getActivity().finish();
         });
-        return binding.getRoot();
+
+        return view;
     }
 
     @Override
